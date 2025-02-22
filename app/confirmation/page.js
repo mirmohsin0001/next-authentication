bdda98ffae8d6a8ecd223f0665b177e97492d0d9
mirmohsin0001 <mirmohsin0001@gmail.com>
2025-02-22 22:27:42 +0530

    stylized the confirmation page

--- app/confirmation/page.js
+++ app/confirmation/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>Form Submitted</div>
-  )
+    return (
+        <div className='h-screen flex items-center justify-center' >
+            <h1 className="text-4xl font-bold ">Form Submitted Successfully !!!</h1>
+        </div>
+    )
 }
 
 export default page
