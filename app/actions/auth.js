2cef34be0fb04e8d6004bb4f5e551d3c7caddb2c
mirmohsin0001 <mirmohsin0001@gmail.com>
2025-02-22 22:19:53 +0530

    use zod for server side form validation

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -1,12 +1,31 @@
 'use server'
 
+import { SignupFormSchema } from '@/app/lib/definitions'
+import { redirect } from 'next/navigation';
 
 export async function signup(formData) {
+    
     // Validate form fields
-    const name = formData.get('name');
-    console.log('name', name);
-    const email = formData.get('email');
-    console.log('email', email);
-    const password = formData.get('password');
-    console.log('password', password);
+    const validatedFields = SignupFormSchema.safeParse({
+        name: formData.get('name'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+    })
+
+
+    // If any form fields are invalid, return early
+    if (!validatedFields.success) {
+        console.log('errors', validatedFields.error.flatten().fieldErrors)
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+
+
+    console.log(validatedFields)
+
+    // If the form is valid, redirect to the confirmation page
+    redirect('/confirmation')
+
+    // Call the provider or db to create a user...
 }
---
+++ app/confirmation/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Form Submitted</div>
+  )
+}
+
+export default page
